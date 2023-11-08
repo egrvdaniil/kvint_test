20b5e6d6c6ff7ec03ccb9e56e9ec3fe6e1cc9bb5
Даниил Егоров <egrvdanil@gmail.com>
2023-11-08 19:59:29 +0400

    add worker startup and shutdown events

--- backend/src/broker_init.py
+++ backend/src/broker_init.py
@@ -1,11 +1,28 @@
 from config import settings
-from result_backend import TasksResultBackend
-from taskiq_aio_pika import AioPikaBroker  # type:ignore
 from database_init import database  # type:ignore
-
+from motor import motor_asyncio
+from result_backend import TasksResultBackend
+from taskiq import TaskiqEvents, TaskiqState
+from taskiq_aio_pika import AioPikaBroker  # type:ignore
 
 result_backend = TasksResultBackend(
     uri=settings.MONGO_URI, database_name=settings.DATABASE_NAME,
 )
 
 broker = AioPikaBroker().with_result_backend(result_backend)
+
+
+@broker.on_event(TaskiqEvents.WORKER_STARTUP)
+async def startup(state: TaskiqState) -> None:
+    db_client = motor_asyncio.AsyncIOMotorClient(
+        settings.MONGO_URI
+    )
+    state.db_client = db_client
+    state.database = db_client.get_database(settings.DATABASE_NAME)
+
+
+@broker.on_event(TaskiqEvents.WORKER_SHUTDOWN)
+async def shutdown(state: TaskiqState) -> None:
+    # Here we close our pool on shutdown event.
+    await state.redis.disconnect()
+    state.db_client.close()
