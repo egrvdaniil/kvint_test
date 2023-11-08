f5de3b0bd95437a973dc956a82f7dd14c6f6bc54
Даниил Егоров <egrvdanil@gmail.com>
2023-11-08 21:14:53 +0400

    add request response models

---
+++ backend/src/api/responses.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+
+class CallDurationCount(BaseModel):
+    sec_10: int
+    sec_10_30: int
+    sec_30: int
+
+
+class CallAggregation(BaseModel):
+    phone: str
+    cnt_all_attempts: int
+    cnt_att_dur: CallDurationCount
+    min_price_att: int
+    max_price_att: int
+    avg_dur_att: float
+    sum_price_att_over_15: float
+
+
+class ReportResponse(BaseModel):
+    data: list[CallAggregation]
+    total_duration: float
+
+
+class TaskStatus(BaseModel):
+    status: str
