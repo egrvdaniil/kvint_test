dfc07ab5b2e4b61c3b21a78e6fb83627eef14c1f
Даниил Егоров <egrvdanil@gmail.com>
2023-11-09 03:58:43 +0400

    add endpoints to setup

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from api.responses import TaskStatusResponse, TaskStatusesListResponse
+
+api_perfix = 'api/v1'
 
 
 def setup_endpoints(app: FastAPI):
-    app.get("/", response_model='')()  # type:ignore
-    app.post("/", response_model='')()  # type:ignore
+    app.get(api_perfix + "/tasks", response_model=TaskStatusesListResponse)()  # type:ignore
+    app.post(api_perfix + "/report", response_model=TaskStatusResponse)()  # type:ignore
