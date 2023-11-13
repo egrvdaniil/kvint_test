ed1f0f646a983c36c4ff5eca83394b50df6aeeb9
Даниил Егоров <egrvdanil@gmail.com>
2023-11-13 12:09:19 +0400

    add test config check

--- backend/src/config.py
+++ backend/src/config.py
@@ -3,6 +3,8 @@ import os
 
 class Settings:
     def __init__(self):
+        if os.environ["IS_TEST"]:
+            return
         self.MONGO_URI = os.environ["MONGO_URI"]
         self.DATABASE_NAME = os.environ["DATABASE_NAME"]
         self.RABBITMQ_URI = os.environ["RABBITMQ_URI"]
